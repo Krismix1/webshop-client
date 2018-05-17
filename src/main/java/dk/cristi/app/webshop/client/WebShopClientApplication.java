70c06c84f96b905ed7240ea330af7dc3530cb07a
Cristian Betivu <cbetivu@gmail.com>
2018-05-17 22:59:35 +0200

    Add eureka client Forwarding to angular homepage

--- src/main/java/dk/cristi/app/webshop/client/WebShopClientApplication.java
+++ src/main/java/dk/cristi/app/webshop/client/WebShopClientApplication.java
@@ -2,6 +2,7 @@ package dk.cristi.app.webshop.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @SpringBootApplication
 @Controller
 @EnableScheduling
+@EnableEurekaClient
 public class WebShopClientApplication {
 
 	@RequestMapping("/")
--- src/main/java/dk/cristi/app/webshop/client/configs/WebConfigDev.java
+++ src/main/java/dk/cristi/app/webshop/client/configs/WebConfigDev.java
@@ -2,12 +2,37 @@ package dk.cristi.app.webshop.client.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
 
 @Profile("dev")
 @Configuration
 public class WebConfigDev implements WebMvcConfigurer {
+
+    // map all requests to the angular app
+    // From https://stackoverflow.com/questions/38516667/springboot-angular2-how-to-handle-html5-urls/46854105#46854105
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**/*")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath,
+                                                   Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                : new ClassPathResource("/static/index.html");
+                    }
+                });
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 //        registry.addMapping("/**"); // Allow CORS for Angular application run as a different application
