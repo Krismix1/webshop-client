c56e2863bd37568f9b18ed9955f76b376e9647c8
Cristian Betivu <cbetivu@gmail.com>
2018-05-27 20:06:37 +0200

    fix imports

--- src/main/java/dk/cristi/app/webshop/client/services/ShoppingCartService.java
+++ src/main/java/dk/cristi/app/webshop/client/services/ShoppingCartService.java
@@ -10,7 +10,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
